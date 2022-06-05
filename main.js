a3dd7cb34a107b38f40711999cee18721d7b57b1
Farrah <60726285+Kwyfarrah@users.noreply.github.com>
2022-06-05 11:36:23 +0900

    use module

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-function showquotes() {
-    var temp = document.getElementsByTagName("template")[0];
-    var clon = temp.content.cloneNode(true);
-    document.body.appendChild(clon);
-  }
+import { showquotes } from './quotes_generators.js';
+
+let generatorsButton = document.getElementById('showquotes');
+generatorsButton.addEventListener('click', showquotes);
---
+++ quotes_generators.js
@@ -0,0 +1,3 @@
+export const showquotes = () => {
+    document.querySelector('p').innerHTML = '"When you have a dream, you\'ve have got to grab it and never let go." <h3>—Carol Burnettr</h3>'
+  }
